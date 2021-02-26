e92d7f1cbaf7cdab174c22dc8d6fb588bd45d7f4
Falk-vm <78727891+Falk-vm@users.noreply.github.com>
2021-02-26 14:36:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pyautogui, time
+import PyAutoGUI, time
 time.sleep(5)
 f = open("ww2.txt", 'r')
 for word in f:
   time.sleep()
-  pyautogui.typewrite(word)
-  pyautogui.press("enter")
+  PyAutoGUI.typewrite(word)
+  PyAutoGUI.press("enter")
